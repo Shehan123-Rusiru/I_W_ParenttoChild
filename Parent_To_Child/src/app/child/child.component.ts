e3d9d48131441e2c9b9bb723aaba1b7977e64e37
Shehan123-Rusiru <ss.rusiru@gmail.com>
2023-01-09 16:45:31 +0530

    Task 01: Parent to Child

--- Parent_To_Child/src/app/child/child.component.ts
+++ Parent_To_Child/src/app/child/child.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.scss']
 })
+
 export class ChildComponent {
+ 
+  @Input() name: string;
+
+  constructor(){
+    this.name = "";
+    //console.log();
+   // alert(this.name);
+  }
 
 }
+
--- Parent_To_Child/src/app/parent/parent.component.ts
+++ Parent_To_Child/src/app/parent/parent.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class ParentComponent {
 
+  MyName = "Shehan Rusiru Senanayake";
 }
